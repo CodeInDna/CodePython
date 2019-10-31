6de61c7a1afd4157f0c2ad07e037aa57ab63535d
ykarelia323@gmail.com <ykarelia323@gmail.com>
2019-10-31 09:37:56 +0530

    rps_intermediate_version

--- Basics/06_rock_paper_scissors_v1.py
+++ Basics/06_rock_paper_scissors_v1.py
@@ -4,22 +4,42 @@ print('***************PAPER****************')
 print('*************SCISSORS***************')
 
 player1 = input("Player 1, make your move : ")
+print('*****NO CHEATING !*****\n\n' * 22)
 player2 = input("Player 2, make your move : ")
 
-
-if player1 == 'rock' and player2 == 'scissors':
-	print('Player 1 wins')
-elif player1 == 'rock' and player2 == 'paper':
-	print('Player 2 wins')
-elif player1 == 'paper' and player2 == 'rock':
-	print('Player 1 wins')
-elif player1 == 'paper' and player2 == 'scissors':
-	print('Player 2 wins')
-elif player1 == 'scissors' and player2 == 'paper':
-	print('Player 1 wins')
-elif player1 == 'scissors' and player2 == 'rock':
-	print('Player 2 wins')
-elif player1 == player2:
+if player1 == player2:
 	print('It\'s a TIE.')
+elif player1 == 'rock':
+	if player2 == 'scissors':
+		print('Player 1 wins')
+	elif player2 == 'paper':
+		print('Player 2 wins')
+elif player1 == 'paper':
+	if player2 == 'rock':
+		print('Player 1 wins')
+	elif player2 == 'scissors':
+		print('Player 2 wins')
+elif player1 == 'scissors':
+	if player2 == 'paper':
+		print('Player 1 wins')
+	elif player2 == 'rock':
+		print('Player 2 wins')		
 else:
 	print('Invalid input!')
+
+# if player1 == 'rock' and player2 == 'scissors':
+# 	print('Player 1 wins')
+# elif player1 == 'rock' and player2 == 'paper':
+# 	print('Player 2 wins')
+# elif player1 == 'paper' and player2 == 'rock':
+# 	print('Player 1 wins')
+# elif player1 == 'paper' and player2 == 'scissors':
+# 	print('Player 2 wins')
+# elif player1 == 'scissors' and player2 == 'paper':
+# 	print('Player 1 wins')
+# elif player1 == 'scissors' and player2 == 'rock':
+# 	print('Player 2 wins')
+# elif player1 == player2:
+# 	print('It\'s a TIE.')
+# else:
+# 	print('Invalid input!')
