0198b395371489fd10f473a3d54ae35a06f4c01a
ykarelia323@gmail.com <ykarelia323@gmail.com>
2019-11-07 23:14:43 +0530

    class attributes

--- Basics/32_oop_class_attr_methods.py
+++ Basics/32_oop_class_attr_methods.py
@@ -37,3 +37,81 @@ user2 = User("Diana","Prince",20)
 print(User.active_user)		#2
 user1.logout()
 print(User.active_user)		#1
+
+
+#More about Class Attributes
+class Pet:
+	allowed = ["cat", "dog", "fish", "rat"]
+	def __init__(self, name, species):
+		if species not in Pet.allowed:
+			raise ValueError(f"You can't have a {species} pet!")
+		self.name = name
+		self.species = species
+
+	def set_species(self, species):		#update species attribute
+		if species not in Pet.allowed:
+			raise ValueError(f"You can't have a {species} pet!")
+		self.species = species
+
+cat = Pet("Princy", "cat")
+dog = Pet("Browny", "dog")
+# tiger = Pet("jfjfh", "tiger")	#ValueError: You can't have a tiger pet!
+
+print("******************************")
+
+# We can update the species directly using below code, but
+# we need to get a better option to update the attributes 
+# so, we gonna use 'set_species' method defined above
+print(dog.species)		#dog
+dog.species = "gibrish"
+print(dog.species)		#gibrish
+
+#Now, we can update the species using a method
+dog.set_species("dog")
+print(dog.species)		#dog
+# dog.set_species("tiger")	#ValueError: You can't have a tiger pet!
+
+
+#Also, We can add more species in allowed variable
+Pet.allowed.append("pig")
+print(Pet.allowed)	#['cat', 'dog', 'fish', 'rat', 'pig'] 
+
+
+#So, defining attributes directly on a class is shared by all instances
+#of a class(cat, dog and Pet shares same id for the allowed attribute)
+#check it out:
+print(id(Pet.allowed))	# 62846344
+print(id(cat.allowed))	# 62846344
+print(id(dog.allowed))	# 62846344
+
+#if we change cat.allowed, it doesn't make any change dog.allowed or Pet.allowed
+#They won't be same thing anymore
+cat.allowed = ['tiger','bear']
+print(cat.allowed)	#['tiger','bear']	
+print(Pet.allowed)	#["cat", "dog", "fish", "rat", "pig"]
+print(dog.allowed)	#["cat", "dog", "fish", "rat", "pig"]
+
+
+#Exercise
+class Chicken:
+
+	total_eggs = 0	# Class Attribute
+
+	def __init__(self, name, species):
+		self.name = name
+		self.species = species
+		self.eggs = 0
+
+	def lay_eggs(self):			# Instance Method
+		self.eggs += 1
+		Chicken.total_eggs += 1
+		return self.eggs
+
+c1 = Chicken(name = "Alice", species = "Partridge Silkie")
+c2 = Chicken(name = "Amelia", species = "Speckled Sussex")
+print(Chicken.total_eggs)	#0
+c1.lay_eggs()
+print(Chicken.total_eggs)	#1
+c2.lay_eggs()
+c2.lay_eggs()
+print(Chicken.total_eggs)	#3
