21fd7a260f326d16a65a932bcb2eca68f67eefd2
ykarelia323@gmail.com <ykarelia323@gmail.com>
2019-11-21 22:35:18 +0530

    Book Scrapper : Saving data to database

--- Basics/58_book_scraper_with_database.py
+++ Basics/58_book_scraper_with_database.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 # url of book store
 url = "http://books.toscrape.com/catalogue/category/books/history_32/index.html"
 
-
+# Extract Data we want
 def scrape_books(url):
 	# Request url
 	response = requests.get(url)
@@ -18,8 +18,17 @@ def scrape_books(url):
 	for book in books:
 		book_data = get_title(book),get_price(book),get_rating(book)
 		all_books.append(book_data)
-	print(all_books)
-	
+	save_books(all_books)
+
+# Save data to database 	
+def save_books(all_books):
+	conn = sqlite3.connect("books.db")
+	c = conn.cursor()
+	# query = "CREATE TABLE books (title TEXT, price REAL, rating INTEGER)"
+	# c.execute(query)
+	c.executemany('INSERT INTO books VALUES(?, ?, ?)', all_books)
+	conn.commit()
+	conn.close()
 
 def get_title(book):
 	return book.find("h3").find("a")["title"]
@@ -34,6 +43,5 @@ def get_rating(book):
 	rating_word = rating_para.get_attribute_list("class")[-1]
 	return ratings[rating_word]
 
-scrape_books(url)
-# Extract Data we want
-# Save data to database 
+# scrape_books(url)	# list of tuples with details about book such as title, price and rating
+
