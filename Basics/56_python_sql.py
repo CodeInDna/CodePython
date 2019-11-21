a65cef37285e404fdd874e81dfed0c0ada1929a1
ykarelia323@gmail.com <ykarelia323@gmail.com>
2019-11-21 21:45:48 +0530

    SQL INJECTION: Bad idea v/s Good idea

--- Basics/56_python_sql.py
+++ Basics/56_python_sql.py
@@ -35,22 +35,43 @@ c = conn.cursor()
 # c.execute(insert_query, (form_first, form_last, form_closeness))
 
 # Bulk inserts in Python*********************
-people = people = [
-	("Roald","Amundsen", 5),
-	("Rosa", "Parks", 8),
-	("Henry", "Hudson", 7),
-	("Neil","Armstrong", 7),
-	("Daniel", "Boone", 3)]
+# people = people = [
+# 	("Roald","Amundsen", 5),
+# 	("Rosa", "Parks", 8),
+# 	("Henry", "Hudson", 7),
+# 	("Neil","Armstrong", 7),
+# 	("Daniel", "Boone", 3)]
 # insert them altogether
 # c.executemany("INSERT INTO friends VALUES(?, ?, ?)", people)
 
 # insert by iterating
-average = 0
-for person in people:
-	# c.execute("INSERT INTO friends VALUES(?, ?, ?)", person)
-	# print("INSERTING DATA")
-	average += person[2]
-print(average/len(people))
+# average = 0
+# for person in people:
+# 	c.execute("INSERT INTO friends VALUES(?, ?, ?)", person)
+# 	print("INSERTING DATA")
+# 	average += person[2]
+# print(average/len(people))
+
+c.execute("SELECT * FROM friends")
+# print(c)		# <sqlite3.Cursor object at 0x0000017B5A8C7960>
+# for result in c:
+# 	print(result)
+# ('Ritu', 'Patel', 6)
+# ...
+# ('Daniel', 'Boone', 3)
+
+# OR
+res = c.fetchall()
+print(res)	# It gives a list of tuples of data
+
+# fetch one based on condition
+c.execute("SELECT * FROM friends WHERE fname IS 'Ritu'")
+res_one = c.fetchone()
+print(res_one)	# ('Ritu', 'Patel', 6)
+
+# select based on closeness and order it in ascending way
+c.execute("SELECT * FROM friends WHERE closeness > 3 ORDER BY closeness")
+print(c.fetchall())
 
 # commit changes
 conn.commit()
